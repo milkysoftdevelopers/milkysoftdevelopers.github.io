0f3b114df64fc23b30662582dd589a040a406b34
Milkysoft Developers <43296536+milkysoftdevelopers@users.noreply.github.com>
2020-08-14 14:25:36 +0500

    Done!

--- jo/script.js
+++ jo/script.js
@@ -1,10 +1,24 @@
 let isWasPaused = false;
 
+alert("Script loaded!!");
+
 document.getElementsByClassName("player-control-play-pause-icon").item(0).onclick = setTimeout(function() {
     isWasPaused = document.getElementsByClassName("player-control-play-pause-icon").item(0).getAttribute("aria-label").toString() === "Play video";
+    if (isWasPaused){
+        var jo = "changed true";
+    }else{
+        var jo = "changed false";
+    }
+    alert(jo);
 },1000)
 
 document.onvisibilitychange = function () {
+     if (isWasPaused){
+        var jo = "true";
+    }else{
+        var jo ="false";
+    }
+    alert(jo);
     if (document.getElementsByClassName("player-control-play-pause-icon").item(0).getAttribute("aria-label").toString() === "Play video" && isWasPaused === false) {
         document.getElementsByClassName("player-control-play-pause-icon").item(0).click();
     }
